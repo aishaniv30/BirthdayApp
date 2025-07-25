5cde3427ab18cfb817be69c0721a42612a44f1a6
Aishani <aishaniv30@gmail.com>
2025-07-25 13:43:48 -0500

    Delete

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -13,14 +13,18 @@ struct ContentView: View {
     @Environment(\.modelContext) private var context
     @State private var newName = ""
     @State private var newBirthday = Date.now
+    
     var body: some View {
         NavigationStack {
-            List(friends) { friend in
-                HStack {
-                    Text(friend.name)
-                    Spacer()
-                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+            List {
+                ForEach(friends) { friend in
+                    HStack {
+                        Text(friend.name)
+                        Spacer()
+                        Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                    }
                 }
+                .onDelete(perform: deleteFriend)
             }
             .navigationTitle("Birthdays")
             .safeAreaInset(edge: .bottom) {
@@ -43,6 +47,12 @@ struct ContentView: View {
                 .padding()
                 .background(.bar)
             }
+        }
+    }
+    func deleteFriend(at offsets: IndexSet) {
+        for index in offsets {
+            let friendtoDelete = friends[index]
+            context.delete(friendtoDelete)
         }
     }
 }
